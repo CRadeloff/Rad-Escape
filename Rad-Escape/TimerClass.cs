c5a0eea5877f3f8cc20e0a9a3828ed023cdf2baa
Connor <Meigs2@users.noreply.github.com>
2018-07-15 19:22:53 -0500

    Moved TimerClass to seprate file.

---
+++ Rad-Escape/TimerClass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Rad_Escape
+{
+    public class TimerClass
+    {
+        private DispatcherTimer Timer = new DispatcherTimer();
+        private DateTime FutureTime = new DateTime();
+        private TimeSpan TimeLeft;
+        private bool TimerIsActive;
+        private string TimeFormat = @"hh\:mm\:ss\.ff";
+        public string VictoryMessage = "You've escaped!";
+        public string DefeatMessage = "Times up!";
+        private MainWindow CurrentWindow;
+
+        public TimerClass(MainWindow mainWindow)
+        {
+            TimerIsActive = false;
+            Timer.Interval = TimeSpan.FromMilliseconds(1);
+            Timer.Tick += Timer_Tick;
+            CurrentWindow = mainWindow;
+        }
+
+        public void SetTimer(int startingHours, int startingMinutes, int startingSeconds)
+        {
+            TimeLeft = new TimeSpan(startingHours, startingMinutes, startingSeconds);
+        }
+
+        public void PauseUnpauseTimer()
+        {
+            if (TimerIsActive) //pause timer and set the timeleft
+            {
+                Timer.Stop();
+                TimeLeft = FutureTime.Subtract(DateTime.Now);
+                TimerIsActive = false;
+            }
+            else //unpause timer
+            {
+                FutureTime = DateTime.Now.Add(TimeLeft);
+                Timer.Start();
+                TimerIsActive = true;
+            }
+        }
+
+        public void TimeUp()
+        {
+            Timer.Stop();
+            return;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (FutureTime <= DateTime.Now)
+            {
+                TimeUp();
+                FutureTime = DateTime.Now;
+                return;
+            }
+            CurrentWindow.CurrentText = FutureTime.Subtract(DateTime.Now).ToString(TimeFormat);
+        }
+
+        public void AddTime(TimeSpan AddTime)
+        {
+            FutureTime += AddTime;
+        }
+    }
+}
